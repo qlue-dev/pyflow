a8b2708179216fdcfd53ca66e1ef06a15cc4d1d3
Yahya <yahya@qlue.id>
2021-06-10 17:06:03 +0700

    Migrate setup.py to use setuptools

--- setup.py
+++ setup.py
@@ -1,24 +1,29 @@
 # Author: Deepak Pathak (c) 2016
 
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-# from __future__ import unicode_literals
-from distutils.core import setup
-from Cython.Build import cythonize
-from distutils.extension import Extension
+from __future__ import absolute_import, division, print_function
+
+import multiprocessing as mp
 from glob import glob
 
 import numpy
+from setuptools import Extension, setup
+from Cython.Build import cythonize
+
+# from __future__ import unicode_literals
 
 sourcefiles = ['pyflow.pyx', ]
 sourcefiles.extend(glob("src/*.cpp"))
-extensions = [Extension("pyflow", sourcefiles, include_dirs=[numpy.get_include()])]
+extensions = [
+    Extension("pyflow", sourcefiles, include_dirs=[numpy.get_include()])]
 setup(
     name="pyflow",
-    version="1.0",
+    version="1.0.1",
     description="Python wrapper for the Coarse2Fine Optical Flow code.",
     author="Deepak Pathak",
-    ext_modules=cythonize(extensions),
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'language_level': 3},
+        nthreads=mp.cpu_count()),
     include_dirs=[numpy.get_include()]
 )
